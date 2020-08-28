51a0b3cafd2a4fe53522f4d469945daca64d0832
Xiaoyi Liu <lxy878@gmail.com>
2020-08-28 15:26:39 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Code your solution in this file!
+
+const returnFirstTwoDrivers = function (array){
+  return array.slice(0, 2);
+}
+
+const returnLastTwoDrivers = function (array){
+  return array.slice(array.length-2);
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+function createFareMultiplier(n){
+  return function(mul){return n*mul;}
+}
+
+// equals createFareMultiplier(2)(10)
+const fareDoubler = createFareMultiplier(2)
+
+// equals createFareMultiplier(3)(12)
+const fareTripler = createFareMultiplier(3)
+
+function selectDifferentDrivers(array, fun){
+  return fun(array);
+}
